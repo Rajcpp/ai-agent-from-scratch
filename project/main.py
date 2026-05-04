e7e97e96dfd4214588f9212e0e9bb01ab3387e6f
Raj <rajmaurya2901@gmail.com>
2026-05-04 22:22:41 +0530

    added memory for later use for llm
    added memory list to store response and result for memory consistency.

--- project/main.py
+++ project/main.py
@@ -4,25 +4,31 @@ from core.input_handler import get_user_command
 
 
 def main():
+    memory = []
     while True:
         response: dict = fake_llm(get_user_command())
         if response is None:
             continue
+
         action = response["action"]
         parameters = response["parameters"]
+
         if action in tools_functions:
             result = tools_functions[action](**parameters)
             print(f"Result: {result}")
+
         elif action == "FINISH":
+            result = "Task finished"
+            memory.append({"llm_response": response, "tool_response": result})
             print("Finishing the session. Goodbye!")
             break
+
         else:
+            result = "Unsupported action"
             print("Error: Unsupported action.")
-        print("-" * 40)
-        print("llm response:", response)
-        print("-" * 40)
-        print("tool_response:", result)
-        print("-" * 40)
+
+        memory.append({"llm_response": response, "tool_response": result})
+        memory.append({"llm_response": response, "tool_response": result})
 
 
 if __name__ == "__main__":
