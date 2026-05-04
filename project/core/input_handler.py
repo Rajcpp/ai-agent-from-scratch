401908c262d4c7c4147e41f756344ba225cde6fa
Raj <rajmaurya2901@gmail.com>
2026-05-04 07:54:07 +0530

    added input handler function fro testing inputs

---
+++ project/core/input_handler.py
@@ -0,0 +1,20 @@
+from config import tools, tools_inputs
+from core.validator import convert_value
+
+
+def get_user_command():
+    action = input("Enter the action you want to perform (e.g., calculate): ")
+    if action == "FINISH":
+        return {"action": "FINISH", "parameters": {}}
+    if action not in tools:
+        print("Error: Unsupported action.")
+        return None
+    parameters = {}
+    for param in tools_inputs[action]:
+        value = input(f"Enter value for {param} (or leave blank to skip): ")
+        converted_value = convert_value(value, tools_inputs[action][param])
+        if value:
+            parameters[param] = converted_value
+        command = {"action": action, "parameters": parameters}
+
+    return command
