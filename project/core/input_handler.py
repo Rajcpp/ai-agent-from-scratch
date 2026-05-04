a67be84ff005e9f085b00e2eb5d8369ced30452d
Raj <rajmaurya2901@gmail.com>
2026-05-04 22:32:02 +0530

    fix unbound command
    removed the command from loop to outside. added dict and none type for return.

--- project/core/input_handler.py
+++ project/core/input_handler.py
@@ -2,19 +2,23 @@ from config import tools, tools_inputs
 from core.validator import convert_value
 
 
-def get_user_command():
+def get_user_command() -> dict | None:
     action = input("Enter the action you want to perform (e.g., calculate): ")
+
     if action == "FINISH":
         return {"action": "FINISH", "parameters": {}}
+
     if action not in tools:
         print("Error: Unsupported action.")
         return None
+
     parameters = {}
+
     for param in tools_inputs[action]:
         value = input(f"Enter value for {param} (or leave blank to skip): ")
         converted_value = convert_value(value, tools_inputs[action][param])
         if value:
             parameters[param] = converted_value
-        command = {"action": action, "parameters": parameters}
 
+    command = {"action": action, "parameters": parameters}
     return command
